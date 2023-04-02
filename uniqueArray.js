7ad37cf8bac47b368652b744eb78e90b68f69577
RitikParikh <ritikparikh98@gmail.com>
2023-04-02 22:50:06 +0530

    RItk:TIme and Space complecty part 1

---
+++ uniqueArray.js
@@ -0,0 +1,35 @@
+const uniqueSlow = (array) => {
+	const newArray = [];
+	
+	for(let i=0;i< array.length; i++){
+		const ele = array[i];
+		if(!newArray.includes(ele)){
+			newArray.push(ele);
+		}
+	}
+	return newArray;
+}
+
+const uniqueFast = (array) => {
+	const onlyUnique = new Set();
+	
+	for(let i=0;i< array.length; i++){
+		onlyUnique.add(array[i]);
+	}
+	return Array.from(onlyUnique);;
+}
+
+const randomArray = [];
+for(let i=0; i< 10000; i++){
+	randomArray.push(Math.floor(Math.random() * 100));
+}
+
+const startSlow = new Date();
+console.log(uniqueSlow(randomArray));
+const endSlow = new Date();
+console.log(`Slow O(n^2) and space O(n) finished in ${endSlow-startSlow} ms.`);
+
+const startFast = new Date();
+console.log(uniqueFast(randomArray));
+const endFast = new Date();
+console.log(`Fast O(n^2) and space O(n) finished in ${endFast-startFast} ms.`);
