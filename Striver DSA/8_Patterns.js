f95b544cac1e78905df235976e35b060667d25de
ritikparikh <ritikparikh98@gmail.com>
2024-02-11 23:44:40 +0530

    Ritik:up to  22

--- Striver DSA/8_Patterns.js
+++ Striver DSA/8_Patterns.js
@@ -205,6 +205,73 @@ const pat15 =()=>{
     }
 }
 
+const pat16 =()=>{
+    let rows =5;
+    for(let i=65; i<=65+rows-1;i++){
+        let str = ``;
+        for(let j=65; j<=i;j++){
+            str += `${String.fromCharCode(i)}`;
+        }
+        console.log(str);
+    }
+}
+
+const pat17 =()=>{
+    let rows =7;
+    for(let i=0; i<rows;i++){
+        let str = "";
+        for(let j=0; j<rows-i-1;j++){
+            str += ` `;
+        }
+        for(let k=65; k<i+1+65;k++){
+            str += `${String.fromCharCode(k)}`;
+        }
+        for(let l=65+i-1; l>=65;l--){
+            str += `${String.fromCharCode(l)}`;
+        }
+        console.log(str);
+    }
+}
+
+const pat18 =()=>{
+    let rows =5;
+    for(let i=rows+64; i>=65;i--){
+        let str = "";
+        for(let j=i; j<=69;j++){
+            str += `${String.fromCharCode(j)}`;
+        }
+        console.log(str);
+    }
+}
+
+const pat21 =()=>{
+    let rows =4;
+    for(let i=1; i<=rows;i++){
+        let str = "";
+        for(let j=1; j<=rows;j++){
+            if(i==1 || i==rows || j==1 || j==rows){
+                str += `*`;
+            }else str +=" "
+        }
+        console.log(str);
+    }
+}
+
+const pat22 =()=>{
+    let rows =4;
+    for(let i=0;i<(2*rows)-1;i++){
+        let str = "";
+        for(let j=0;j<(2*rows)-1;j++){
+            let top = i;
+            let bottom = j;
+            let right = (2*rows - 2) - j;
+            let left = (2*rows - 2) - i;
+            str += `${rows-Math.min(Math.min(top,bottom),Math.min(left,right))}`;
+        }
+        console.log(str);
+    }
+}
+
 // pat1();
 // pat2();
 // pat3();
@@ -219,4 +286,9 @@ const pat15 =()=>{
 // pat12();
 // pat13();
 // pat14();
-pat15();
+// pat15();
+// pat16();
+// pat17();
+// pat18();
+// pat21();
+pat22();
