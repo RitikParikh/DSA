c253819445d4235d80286c61223a614a8b9fd899
ritikparikh <ritikparikh98@gmail.com>
2024-02-18 12:56:47 +0530

    Ritik:10 reverse number

--- Striver DSA/10_reverse_number.js
+++ Striver DSA/10_reverse_number.js
@@ -19,3 +19,23 @@ Constraints:
 -231 <= x <= 231 - 1 */
 
 
+function reverse(num) {
+    let revNum =0;
+    let x=num;
+    const INT_MAX = Math.pow(2, 31) - 1;
+       const INT_MIN = -Math.pow(2, 31);
+    while(x!=0){
+    let digit = Math.floor(x%10);
+     if (revNum > INT_MAX / 10 || (revNum === INT_MAX / 10 && digit > 7)) {
+               return 0;
+           }
+           if (revNum < INT_MIN / 10 || (revNum === INT_MIN / 10 && digit < -8)) {
+               return 0;
+           }
+    revNum = revNum*10 + digit;
+    x=Math.trunc(x/10);
+    }
+    return revNum;
+   };
+
+   reverse(12122);
