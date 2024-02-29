aa37922e3f69ea8cd66ebf5af6e01c976e617112
ritikparikh <ritikparikh98@gmail.com>
2024-03-01 00:19:05 +0530

    Ritik:20_plaindrome_leetcode

--- Striver DSA/20_plaindrome_recurrsion.js
+++ Striver DSA/20_plaindrome_recurrsion.js
@@ -3,10 +3,17 @@
 Given a string s, return true if it is a palindrome, or false otherwise. */
 
 
-function isPalindrome(s,i=0) {
+function checkPlaindrome(s,i) {
     if(i >= s.length/2) return true;
     if(s[i] != s[s.length-i-1]) return false;
-    return isPalindrome(s,i+1);
+    return checkPlaindrome(s,i+1);
+}
+function isPalindrome(s) {
+    let str = s.toLowerCase();
+    let pattern = /[^a-z0-9]/g;
+    str =  str.replace(pattern, '');
+    console.log(str);
+    return checkPlaindrome(str,0);
 };
 
-console.log(isPalindrome("chromeewwmorhc"));
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
